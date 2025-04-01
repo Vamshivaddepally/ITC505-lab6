37bf6a024ef6879157265b9176d90db0336526c7
Vamshivaddepally <vv662@nau.edu>
2025-03-31 17:02:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Interactivity example: Changing text on button click
+document.getElementById("changeTextBtn").addEventListener("click", function() {
+    document.getElementById("dynamicText").textContent = "You clicked the button! Here is some dynamic text.";
+});
